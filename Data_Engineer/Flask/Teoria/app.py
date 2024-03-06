3d00d83b8094ec18d01f2d01ddf8ce53b7d7c72d
RaulTaylor1 <145385966+RaulTaylor1@users.noreply.github.com>
2024-03-06 21:20:21 +0100

    flask venv

---
+++ Data_Engineer/Flask/Teoria/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def indexo():
+    return "<h1>Hello World!</h1>"
+
+@app.route('/rodrigo')
+def index1():
+    return "<h1>wasa<h1>"
+
+
+@app.route("/user/<name>/<int:index>")
+def index(name, index):
+    mylist = ['elemento1', 'elemento2', 'elemento3', 'elemento4']
+    mydict = {'key': 'valor'}
+    mytuple = ('tuple1', 'tuple2', 'tuple3', 'tuple4')
+    return name, index, mylist, mydict, mytuple
